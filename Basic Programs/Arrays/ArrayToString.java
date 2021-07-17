9d7278310ea121376651ed7b4e2b662fe95fcb78
lakshmipriya1215 <69707626+lakshmipriya1215@users.noreply.github.com>
2021-07-17 16:04:09 +0530

    Add files via upload

---
+++ Basic Programs/Arrays/ArrayToString.java
@@ -0,0 +1,12 @@
+public class ArrayToString
+{
+   public static void main(String args[])
+   {
+      // Method 1: Using String object
+      char[] ch = {'s','t','r','a','w','b','e','r','r','i','e','s'};
+      String str = new String(ch);
+      System.out.println(str);
+ 
+      
+   }
+}
